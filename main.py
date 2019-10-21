735ff087a9b66f1d5490a099d31b5946077cd3c7
Kirill Stoyanov <kyrylo.stoianov@nure.ua>
2019-10-21 17:36:36 +0300

    Calculation of minimum and maximum for each attribute has been added, the names of variables were also changed

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 import csv
 import statistics
+# data set "wine" has been converted to a list
 with open('wine.data', 'r') as f:
     wines = list(csv.reader(f))
+#avg value of each atribute
 avg_list = []
-for j in range(0, 13):
+for j in range(1, 14):
     avg = [float(i[j]) for i in wines]
     sum_score = sum(avg)
     num_score = len(avg)
     avg_list.append(sum_score/num_score)
 print(avg_list)
+#medain for each atribute
 mode_list = []
-for j in range(0, 13):
-    moda = [float(i[j]) for i in wines if i[j] != ""]
-    num_wines = len(moda)
-    sorted_prices = sorted(moda)
+for j in range(1, 14):
+    median = [float(i[j]) for i in wines if i[j] != ""]
+    num_wines = len(median)
+    sorted_prices = sorted(median)
     mode_list.append(statistics.median(sorted_prices))
 print(mode_list)
+#minimum and maximum for each atribute
+min_score = []
+max_score = []
+for j in range(1, 14):
+    scores = [float(i[j]) for i in wines if i[j] != ""]
+    max_score.append(max(scores))
+    min_score.append(min(scores))
+print(min_score, '\n', max_score)
