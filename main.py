e73a16cc01b7859dc9d58ccc2e227c6a97016dfc
Kirill Stoyanov <kyrylo.stoianov@nure.ua>
2019-10-21 17:24:57 +0300

    The calculation of the mode of each attribute was implemented, the names of variables were also changed

--- main.py
+++ main.py
@@ -1,29 +1,18 @@
-# import csv
-# with open('wine.data', 'r') as f:
-#     wines = list(csv.reader(f))
-#
-# scores = [float(i[0]) for i in wines]
-# sum_score = sum(scores)
-# num_score = len(scores)
-# avg_score = sum_score/num_score
-# print(avg_score)
-#
-# scores = []
-# for i in wines:
-#     x = float[i]
-#     sum_score = sum(x)
-#     num_score = len(x)
-#     i += 1
-# print(scores)
-
 import csv
+import statistics
 with open('wine.data', 'r') as f:
     wines = list(csv.reader(f))
-avg_score = []
+avg_list = []
 for j in range(0, 13):
-    scores = [float(i[j]) for i in wines]
-    sum_score = sum(scores)
-    num_score = len(scores)
-    avg_score.append(sum_score/num_score)
-print(avg_score)
-
+    avg = [float(i[j]) for i in wines]
+    sum_score = sum(avg)
+    num_score = len(avg)
+    avg_list.append(sum_score/num_score)
+print(avg_list)
+mode_list = []
+for j in range(0, 13):
+    moda = [float(i[j]) for i in wines if i[j] != ""]
+    num_wines = len(moda)
+    sorted_prices = sorted(moda)
+    mode_list.append(statistics.median(sorted_prices))
+print(mode_list)
