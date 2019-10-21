20331ac6aa19a2ffd8cd455b5a4b0d5e580a121b
Kirill Stoyanov <kyrylo.stoianov@nure.ua>
2019-10-21 18:23:06 +0300

    The scope was calculated for each attribute

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import csv
 import statistics
+import math
 # data set "wine" has been converted to a list
 with open('wine.data', 'r') as f:
     wines = list(csv.reader(f))
-#avg value of each atribute
+#avg value of each attribute
 avg_list = []
 for j in range(1, 14):
     avg = [float(i[j]) for i in wines]
@@ -11,15 +12,15 @@ for j in range(1, 14):
     num_score = len(avg)
     avg_list.append(sum_score/num_score)
 print(avg_list)
-#medain for each atribute
-mode_list = []
+#medain for each attribute
+median_list = []
 for j in range(1, 14):
     median = [float(i[j]) for i in wines if i[j] != ""]
     num_wines = len(median)
     sorted_prices = sorted(median)
-    mode_list.append(statistics.median(sorted_prices))
-print(mode_list)
-#minimum and maximum for each atribute
+    median_list.append(statistics.median(sorted_prices))
+print(median_list)
+#minimum and maximum for each attribute
 min_score = []
 max_score = []
 for j in range(1, 14):
@@ -27,3 +28,29 @@ for j in range(1, 14):
     max_score.append(max(scores))
     min_score.append(min(scores))
 print(min_score, '\n', max_score)
+#dispersion for each attribute
+dis_list = []
+for j in range(1, 14):
+    scores = [float(i[j]) for i in wines]
+    sum_score = sum(scores)
+    num_score = len(scores)
+    m = sum(scores) / len(scores)
+    dis_list.append(sum((xi - m) ** 2 for xi in scores) / len(scores))
+print(dis_list)
+#standard deviation for each attribute
+deviation_list = []
+for j in range(1, 14):
+    deviation = [float(i[j]) for i in wines]
+    sum_score = sum(deviation)
+    num_score = len(deviation)
+    m = sum(deviation) / len(deviation)
+    deviation_list.append(math.sqrt(sum((xi - m) ** 2 for xi in deviation) / len(deviation)))
+print(deviation_list)
+#scope for each attribute
+scope = []
+for j in range(1, 14):
+    scores = [float(i[j]) for i in wines]
+    scope.append(max(scores) - min(scores))
+print(scope)
+
+
